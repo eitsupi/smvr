410e1c1cc602b0f806ee72c7ac4f5fae63b6c0d3
eitsupi <ts1s1andn@gmail.com>
2025-06-21 13:57:56 +0000

    feat: add functions to update versions

---
+++ R/update.R
@@ -0,0 +1,120 @@
+#' Update version components
+#'
+#' @description
+#' These functions allows to update the components of version objects.
+#'
+#' - `increment_major()`, `increment_minor()`, and `increment_patch()` update
+#'   the major, minor, and patch version numbers respectively.
+#' - `mark_as_pre_release()` marks the version as a pre-release version.
+#' - `add_build_metadata()` adds build metadata to the version.
+#'
+#' @param x An version object
+#' @param ... Additional arguments passed to methods.
+#' @param ids A character vector of pre-release identifiers.
+#' @param metadata A character vector of build metadata.
+#' @return An updated version object with the specified changes applied.
+#' @examples
+#' v <- parse_semver(c("0.9.9", "1.0.0-a.1", "1.1.0+1"))
+#'
+#' increment_major(v)
+#' increment_minor(v)
+#' increment_patch(v)
+#' mark_as_pre_release(v, ids = c("pre.1"))
+#' add_build_metadata(v, metadata = "build.1")
+#' @name update-version
+NULL
+
+#' @rdname update-version
+#' @export
+increment_major <- function(x, ...) {
+  UseMethod("increment_major")
+}
+
+#' @rdname update-version
+#' @export
+increment_major.smvr <- function(x, ...) {
+  core <- field(x, "version_core")
+  smvr(
+    major = core$major + 1L,
+    minor = 0L,
+    patch = 0L,
+    pre_release = "",
+    build = ""
+  )
+}
+
+#' @rdname update-version
+#' @export
+increment_minor <- function(x, ...) {
+  UseMethod("increment_minor")
+}
+
+#' @rdname update-version
+#' @export
+increment_minor.smvr <- function(x, ...) {
+  core <- field(x, "version_core")
+  smvr(
+    major = core$major,
+    minor = core$minor + 1L,
+    patch = 0L,
+    pre_release = "",
+    build = ""
+  )
+}
+
+#' @rdname update-version
+#' @export
+increment_patch <- function(x, ...) {
+  UseMethod("increment_patch")
+}
+
+#' @rdname update-version
+#' @export
+increment_patch.smvr <- function(x, ...) {
+  core <- field(x, "version_core")
+  smvr(
+    major = core$major,
+    minor = core$minor,
+    patch = core$patch + 1L,
+    pre_release = "",
+    build = ""
+  )
+}
+
+#' @rdname update-version
+#' @export
+mark_as_pre_release <- function(x, ...) {
+  UseMethod("mark_as_pre_release")
+}
+
+#' @rdname update-version
+#' @export
+mark_as_pre_release.smvr <- function(x, ids = "pre", ...) {
+  ids <- vec_cast(ids, pre_release_ids())
+  # Recycle ids if the length is 1
+  field(x, "pre_release") <- if (length(ids) == 1L) {
+    rep(ids, length(x))
+  } else {
+    ids
+  }
+  x
+}
+
+#' @rdname update-version
+#' @export
+add_build_metadata <- function(x, ...) {
+  UseMethod("add_build_metadata")
+}
+
+#' @rdname update-version
+#' @export
+add_build_metadata.smvr <- function(x, metadata = "", ...) {
+  metadata <- vec_cast(metadata, character())
+  # Recycle metadata if the length is 1
+  field(x, "build") <- if (length(metadata) == 1L) {
+    rep(metadata, length(x))
+  } else {
+    metadata
+  }
+  x
+}
---
+++ tests/testthat/test-update.R
@@ -0,0 +1,47 @@
+test_that("update functions work", {
+  smvr_vec <- parse_semver(
+    c("1.2.3", "1.2.3-a.1", "2.0.0+foo.bar")
+  )
+  expect_equal(
+    increment_major(smvr_vec),
+    smvr(c(2, 2, 3))
+  )
+  expect_equal(
+    increment_minor(smvr_vec),
+    smvr(c(1, 1, 2), c(3, 3, 1))
+  )
+  expect_equal(
+    increment_patch(smvr_vec),
+    smvr(c(1, 1, 2), c(2, 2, 0), c(4, 4, 1))
+  )
+  expect_equal(
+    mark_as_pre_release(smvr_vec, "beta"),
+    smvr(
+      c(1, 1, 2),
+      c(2, 2, 0),
+      c(3, 3, 0),
+      "beta",
+      c("", "", "foo.bar")
+    )
+  )
+  expect_equal(
+    add_build_metadata(smvr_vec, "build.1"),
+    smvr(
+      c(1, 1, 2),
+      c(2, 2, 0),
+      c(3, 3, 0),
+      c("", "a.1", ""),
+      "build.1"
+    )
+  )
+  expect_equal(
+    increment_major(smvr_vec) |>
+      mark_as_pre_release(c("alpha", "beta", "rc")) |>
+      add_build_metadata(c("a", "b", "c")),
+    smvr(
+      c(2, 2, 3),
+      pre_release = c("alpha", "beta", "rc"),
+      build = c("a", "b", "c")
+    )
+  )
+})
